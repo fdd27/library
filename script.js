558e3d93394b081e3cdb767d72b0bdc949311b07
fdd <fddfddfddfddfddfdd@gmail.com>
2021-06-30 15:18:46 +0300

    Added basic functionality for adding and removing books and displaying book info. Yet to add searching, sorting and storing data

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function Book(title, author, pages, completed, cover) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.comleted = completed;
+    this.completed = completed;
     this.cover = cover;
 }
 
@@ -38,7 +38,54 @@ function showBooks() {
         const bookCard = document.createElement('div');
         container.appendChild(bookCard);
         bookCard.classList.add('book');
-        bookCard.style.cssText = `background: url('${book.cover}'); background-repeat: no-repeat; background-size: 280px 400px;`;
+        bookCard.style.cssText = `background: url('${book.cover}'); background-repeat: no-repeat; background-size: 340px 500px;`;
+
+        const formBook = document.createElement('form');
+        bookCard.appendChild(formBook);
+        formBook.classList.add('formBook');
+        
+        formBook.innerHTML = `<ul>
+                            <li><p>Title:</p><label>${book.title}</label></li>
+                            <li><p>Author:</p><label>${book.author}</label></li>
+                            <li><p>Pages:</p><label>${book.pages}</label></li>
+                            <li><p>Read:</p></li>
+                            </ul>`;
+        
+        bookCard.addEventListener('click', () => {
+            formBook.style.display = 'block';
+        });
+
+        chkRead = document.createElement('input');
+        chkRead.type = "checkbox";
+        formBook.appendChild(chkRead);
+        chkRead.classList.add('chkRead');
+        if (book.completed) chkRead.checked = true;
+        else chkRead.checked = false;
+        chkRead.addEventListener('change', function() {
+            if (this.checked) {
+                book.completed = true;
+                console.log(book.completed);
+            } else {
+                book.completed = false;
+                console.log(book.completed);
+            }
+        });
+
+        btnDelete = document.createElement('button');
+        formBook.appendChild(btnDelete);
+        btnDelete.classList.add('btnDelete');
+        btnDelete.innerText = 'Delete';
+        btnDelete.addEventListener('click', () => {
+            container.removeChild(bookCard);
+        });
+
+        btnHide = document.createElement('button');
+        formBook.appendChild(btnHide);
+        btnHide.classList.add('btnHide');
+        btnHide.innerText = 'Hide';
+        btnHide.addEventListener('click', () => {
+            formBook.style.display = 'none';
+        });
     });
 }
 
@@ -46,13 +93,13 @@ function submitBook() {
     inTitle = document.querySelector('#txtTitle').value;
     inAuthor = document.querySelector('#txtAuthor').value;
     inPages = document.querySelector('#txtPages').value;
-    inCompleted = document.querySelector('#chkCompleted').value;
+    inCompleted = document.querySelector('#chkCompleted').checked;
     inCover = document.querySelector('#txtCover').value;
-
-    console.log(inTitle, inAuthor, inPages, inCompleted, inCover); //log
-
-    addBookToLibrary(inTitle, inAuthor, inPages, true, inCover);
+    console.log(inCompleted); //log
+    addBookToLibrary(inTitle, inAuthor, inPages, inCompleted, inCover);
     showBooks();
+    //clearForm();
+    closeForm();
 }
 
 function openForm() {
@@ -61,4 +108,12 @@ function openForm() {
 
 function closeForm() {
     document.querySelector('#formAdd').style.display = 'none';
+}
+
+function clearForm() {
+    document.querySelector('#txtTitle').value = '';
+    document.querySelector('#txtAuthor').value = '';
+    document.querySelector('#txtPages').value = '';
+    document.querySelector('#chkCompleted').value = '';
+    document.querySelector('#txtCover').value = '';
 }
